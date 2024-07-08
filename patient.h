812f8158b77339360c3f8c875444d3b2ed3e0e6d
Thiago Fabiano <110238019+ThiagoFabiano@users.noreply.github.com>
2024-07-07 22:36:37 -0300

    commit

--- patient.h
+++ patient.h
@@ -5,10 +5,16 @@
 
 typedef struct patient Patient;
 
+
+//Cria um novo paciente.
 Patient* create_patient(int id, const char *name, struct tm *birthdate);
+//Libera a memória alocada para a estrutura do paciente.
 void destroy_patient(Patient *patient);
+//Retorna a identificação única do paciente.
 int get_patient_id(Patient *patient);
+//Retorna o nome do paciente.
 const char* get_patient_name(Patient *patient);
+// Retorna a data de nascimento do paciente.
 struct tm* get_patient_birthdate(Patient *patient);
 
 #endif
