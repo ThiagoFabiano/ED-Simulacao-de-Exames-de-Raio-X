7e0b73804fe3cbda4960e5cb2d9644706a64e429
Thiago Fabiano <110238019+ThiagoFabiano@users.noreply.github.com>
2024-07-07 22:38:59 -0300

    commit

--- exam.h
+++ exam.h
@@ -6,11 +6,17 @@
 
 typedef struct exam Exam;
 
+// Cria um novo exame
 Exam* create_exam(int id, int patient_id, int rx_id, struct tm *time);
+// Libera a memória alocada para a estrutura de exame
 void destroy_exam(Exam *exam);
+// Retorna a identificação única do exame
 int get_exam_id(Exam *exam);
+// Retorna a identificação única do paciente que realizou o exame
 int get_exam_patient_id(Exam *exam);
+// Retorna a identificação única do aparelho de raio-X utilizado no exame
 int get_exam_rx_id(Exam *exam);
+// Retorna o horário de realização do exame
 struct tm* get_exam_time(Exam *exam);
 
 #endif
