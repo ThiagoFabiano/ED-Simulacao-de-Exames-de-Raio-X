dbf7a47268fdc24dd06bdf0c22fa056f1670e2a9
liviahombre <152039280+liviahombre@users.noreply.github.com>
2024-07-07 22:28:50 -0300

    Create patient.h

---
+++ patient.h
@@ -0,0 +1,14 @@
+#ifndef PATIENT_H
+#define PATIENT_H
+
+#include <time.h>
+
+typedef struct patient Patient;
+
+Patient* create_patient(int id, const char *name, struct tm *birthdate);
+void destroy_patient(Patient *patient);
+int get_patient_id(Patient *patient);
+const char* get_patient_name(Patient *patient);
+struct tm* get_patient_birthdate(Patient *patient);
+
+#endif
